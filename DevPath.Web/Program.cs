4842cfca1dc1bb2d1b43e887e17aacf4ded45bea
Totuhov <nikolaytotuhov@gmail.com>
2024-06-25 09:39:59 +0200

    Add: SQLite

--- DevPath.Web/Program.cs
+++ DevPath.Web/Program.cs
@@ -11,9 +11,9 @@ namespace DevPath.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("Sqlite") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseSqlite(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
